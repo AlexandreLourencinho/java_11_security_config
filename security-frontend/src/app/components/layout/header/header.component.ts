6a2c48e475484de00dfb020c6bf51da40dc404a6
Alexandre LOURENCINHO <alexandre.lourencinho@cgi.com>
2023-08-13 00:34:42 +0200

    [feature] : changed the language choice logic in the header navbar

--- security-frontend/src/app/components/layout/header/header.component.ts
+++ security-frontend/src/app/components/layout/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {TranslateService} from "@ngx-translate/core";
 import {Router} from "@angular/router";
 
@@ -7,22 +7,46 @@ import {Router} from "@angular/router";
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
 
   constructor(private translate: TranslateService, private router: Router) {
   }
 
+  ngOnInit(): void {
+    this.language = this.translate.currentLang;
+    console.log('this.translate.getLangs()', this.translate.getLangs(), 'at', new Date().toLocaleTimeString())
+    console.log('this.translate.current', this.translate.currentLang, 'at', new Date().toLocaleTimeString())
+  }
+
+  public language: any;
+
   goToLogin() {
     this.router.navigate(["/login"]);
   }
 
+  goToHomePage() {
+    this.router.navigate(['']);
+  }
+
   translateEn() {
     this.translate.use('en')
-  }
+  }// TODO delete ?
 
   translateFr() {
     this.translate.use('fr')
+  } // todo delete ?
+
+  getTranslateService(): TranslateService {
+    return this.translate;
   }
 
+  changeEffect() {
+    console.log('this.language', this.language, 'at', new Date().toLocaleTimeString());
+    if(this.language !== this.translate.currentLang) {
+      console.info("lang change triggered")
+      this.translate.use(this.language);
+      console.log('this.translate.currentLang', this.translate.currentLang, 'at', new Date().toLocaleTimeString())
+    }
+  }
 }
