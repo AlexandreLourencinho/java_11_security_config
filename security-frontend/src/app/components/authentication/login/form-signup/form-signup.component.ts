5432d8c00e389fe852c5105c456060d6305a1635
Alexandre LOURENCINHO <alexandre.lourencinho@cgi.com>
2023-08-13 00:39:57 +0200

    [feature] : added small signup logic in the form-login component and translations with card-themed material design - not definitive

--- security-frontend/src/app/components/authentication/login/form-signup/form-signup.component.ts
+++ security-frontend/src/app/components/authentication/login/form-signup/form-signup.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthenticationService} from "@services/authentication/authentication.service";
+import SignupRequestModel from "@app/models/authentication/signup-request.model";
 
 @Component({
   selector: 'app-form-signup',
@@ -6,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-signup.component.css']
 })
 export class FormSignupComponent implements OnInit {
+  password: string | undefined;
+  email: string | undefined;
+  username: string | undefined;
+  confirmPassword: string | undefined;
 
-  constructor() { }
+  constructor(private authentService: AuthenticationService) {
+  }
 
   ngOnInit(): void {
   }
 
+  /**
+   *
+   */
+  public signup() {
+    if (
+      this.username
+      && this.password
+      && this.confirmPassword
+      && this.email
+      && this.password.trim() === this.confirmPassword.trim()
+    ) {
+      console.log("username = " + this.username + " password=" + this.password + " email=" + this.email);
+      const signupRequest: SignupRequestModel = new SignupRequestModel(this.username, this.password, this.email);
+      this.authentService.getSignupService().signupUser(signupRequest); // TODO
+    }
+  }
 }
