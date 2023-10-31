846fcf91aab72f84d257f43ad348954edf3c80d8
Alexandre <alexandre.lourencinho@gmail.com>
2023-10-31 16:52:34 +0100

    [feat] unit test with jest in authentication.service.spec.ts

--- security-frontend/src/app/services/authentication/authentication.service.spec.ts
+++ security-frontend/src/app/services/authentication/authentication.service.spec.ts
@@ -1,16 +1,34 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthenticationService } from './authentication.service';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {SignupService} from "@services/authentication/signup.service";
+import {LoginService} from "@services/authentication/login.service";
+import {HttpClient} from "@angular/common/http";
 
 describe('AuthenticationService', () => {
   let service: AuthenticationService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        {provide: HttpClient, useClass: HttpClientTestingModule},
+        SignupService,
+        LoginService
+      ]
+    });
     service = TestBed.inject(AuthenticationService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should get login service', () => {
+    expect(service.getLoginService()).toBeDefined();
+  });
+
+  it('should get signup service', () => {
+    expect(service.getSignupService()).toBeDefined();
   });
 });
